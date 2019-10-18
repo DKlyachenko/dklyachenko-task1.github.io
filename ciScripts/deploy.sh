759c2ffab1d32d3415910f0fe3b11f32dd3ba57c
dklyachenko <dklyachenko@gmail.com>
2019-10-18 07:56:05 +0500

    fix script

--- ciScripts/deploy.sh
+++ ciScripts/deploy.sh
@@ -3,7 +3,7 @@ set -e
 echo "Pull request: ${TRAVIS_PULL_REQUEST}"
 echo "Parameter: ${1}"
 
-if [["${TRAVIS_PULL_REQUEST}" == "false"]]
+if [["${TRAVIS_PULL_REQUEST}" = "false"]]
 then
     echo "Not PR"
     if [["${1}" == "master"]] || [["${1}" == "develop"]]
