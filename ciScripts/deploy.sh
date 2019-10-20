f3bd7c37a89afe840d8b8702f18695be61b1a029
dklyachenko <dklyachenko@gmail.com>
2019-10-20 09:49:48 +0500

    add deploy script

--- ciScripts/deploy.sh
+++ ciScripts/deploy.sh
@@ -9,6 +9,36 @@ then
     if [[ "${1}" == "master" ]] || [[ "${1}" == "develop" ]]
     then
         echo "Correct branch"
+        repositoryAddress = "DKlyachenko/dklyachenko-task1.github.io"
+
+        echo "Prepare for deploy to gh-pages."
+        echo $PWD
+        echo "Clone ${repositoryAddress} to gh-pages"
+        git clone --recursive "https://github.com/${repositoryAddress}.git" repo
+        cd repo
+        echo $PWD
+
+        git checkout gh-pages
+        git pull
+
+        echo "Remove results of previous deploy for ${TRAVIS_BRANCH} branch"
+        rm -rf "${TRAVIS_BRANCH}"
+        mkdir "${TRAVIS_BRANCH}"
+
+        echo "Copy application for ${TRAVIS_BRANCH} branch"
+        cp -r ../docs/* "${TRAVIS_BRANCH}"
+        cd "${TRAVIS_BRANCH}"
+        ls -l
+        cd ..
+
+        git config user.name "dklyachenko"
+        git config user.email "dklyachenko@gmail.com"
+
+        echo "Commit & push changes"
+        git add --all
+        git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
+
+        git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryAddress}.git" > /dev/null 2>&1
     fi
 fi
 echo "Deploy finished"
