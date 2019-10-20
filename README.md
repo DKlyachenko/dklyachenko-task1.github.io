8a9f7fdb4de03e2ca23e275b91a9a56d7cb3fef2
DKlyachenko <43580468+DKlyachenko@users.noreply.github.com>
2019-10-20 10:47:55 +0500

    merge develop to master (#14)
    merge develop to master

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # developersSchool-Task1
 Module 1 of Developers School
+
+You automatically receive invoices from guarded-fortress-00255.herokuapp.com.
+
+To create a new invoice click "Add new" button, fill the information and save changes.
+
+To delete invoice click "Delete" button.
+
+To edit the invoice click "Edit" button, change the information and save changes.
+
+You can filter and sort invoices on the main screen.
---
+++ ciScripts/deploy.sh
@@ -0,0 +1,44 @@
+set -e
+
+echo "Pull request: ${TRAVIS_PULL_REQUEST}"
+echo "Parameter: ${1}"
+
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]
+then
+    echo "Not PR"
+    if [[ "${1}" == "master" ]] || [[ "${1}" == "develop" ]]
+    then
+        echo "Correct branch"
+        repositoryRelativeGitHubAddress="DKlyachenko/dklyachenko-task1.github.io"
+
+        echo "Prepare for deploy to gh-pages."
+        echo $PWD
+        echo "Clone ${repositoryRelativeGitHubAddress} to gh-pages"
+        git clone --recursive "https://github.com/${repositoryRelativeGitHubAddress}.git" repo
+        cd repo
+        echo $PWD
+
+        git checkout gh-pages
+        git pull
+
+        echo "Remove results of previous deploy for ${TRAVIS_BRANCH} branch"
+        rm -rf "${TRAVIS_BRANCH}"
+        mkdir "${TRAVIS_BRANCH}"
+
+        echo "Copy application for ${TRAVIS_BRANCH} branch"
+        cp -r ../docs/* "${TRAVIS_BRANCH}"
+        cd "${TRAVIS_BRANCH}"
+        ls -l
+        cd ..
+
+        git config user.name "dklyachenko"
+        git config user.email "dklyachenko@gmail.com"
+
+        echo "Commit & push changes"
+        git add --all
+        git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
+
+        git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryRelativeGitHubAddress}.git" > /dev/null 2>&1
+    fi
+fi
+echo "Deploy finished"
